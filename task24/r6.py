b87e1963c545308f96185f044c891b9226073bee
Руслан Аитов <r_aitov@mail.ru>
2023-01-13 16:09:09 +0300

    task 24

---
+++ task24/r6.py
@@ -0,0 +1,22 @@
+s = open('k8-0.txt').read()
+max_len = 1
+cur_len = 1
+for i in range(1, len(s)):
+    c1, c2 = s[i - 1], s[i]
+    if c1 == c2:
+        cur_len += 1
+        if cur_len > max_len:
+            max_len = cur_len
+    else:
+        cur_len = 1
+print(max_len)
+
+cur_len = 1
+for i in range(1, len(s)):
+    c1, c2 = s[i - 1], s[i]
+    if c1 == c2:
+        cur_len += 1
+        if cur_len == max_len:
+            print(c1)
+    else:
+        cur_len = 1
--- task24/r6_new.py
+++ task24/r6_new.py
@@ -1,16 +1,16 @@
-s = open('24.txt').read()
-# print(s)
-max_len = 1
+s = open('k8-0.txt').read()
+a = [s[0]]
 cur_len = 1
+max_len = 1
 for i in range(1, len(s)):
     c1, c2 = s[i - 1], s[i]
-    if c1 != c2:
+    if c1 == c2:
         cur_len += 1
         if cur_len > max_len:
             max_len = cur_len
+            a = [c2]
+        elif cur_len == max_len:
+            a.append(c2)
     else:
         cur_len = 1
-print(max_len)
-
-
-# 123
+print(max_len, a)
