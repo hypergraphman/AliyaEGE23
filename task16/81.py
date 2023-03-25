133f99a875bbb59c82dc224acc091f844b27628d
Руслан Аитов <r_aitov@mail.ru>
2023-03-25 15:28:08 +0300

    task 16

---
+++ task16/81.py
@@ -0,0 +1,16 @@
+def f(n):
+    if n <= 5:
+        return n
+    if n > 5 and n % 5 == 0:
+        return n + f(n // 5 + 1)
+    if n > 5 and n % 5 != 0:
+        return n + f(n + 6)
+
+
+for i in range(1000):
+    try:
+        if f(i) > 1000:
+            print(i)
+            break
+    except:
+        pass
