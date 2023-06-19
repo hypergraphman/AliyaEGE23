f9f871ceb23bf1681fda2519698ef627e7c64ec5
Руслан Аитов <r_aitov@mail.ru>
2023-06-19 21:15:27 +0300

    razbor

--- task24/ЕГЭ.py
+++ task24/ЕГЭ.py
@@ -1,10 +1,5 @@
 s = open('24gen.txt').read()
-a = [len(x) for x in s.split('T')][1:-1]
-min_s = 10**10
-for i in range(len(a)-210):
-    min_s = min(min_s, sum(a[i:i + 209]) + 210)
-print(min_s)
-
+# если ищем минальную длину подстроки содержащую 210 символов Т
 i_t = []
 for i in range(len(s)):
     if s[i] == 'T':
@@ -13,3 +8,13 @@ min_L = 10**20
 for i in range(210, len(i_t)):
     min_L = min(min_L, i_t[i] - i_t[i - 209])
 print(min_L+1)
+
+# если ищем максимальную длину подстроки содержащую 210 символов Т
+i_t = []
+for i in range(len(s)):
+    if s[i] == 'T':
+        i_t.append(i)
+mx = 0 # изменение тут
+for i in range(212, len(i_t)): # изменение тут
+    mx = max(mx, i_t[i] - i_t[i - 211]) # Тут
+print(mx - 1) # и тут
